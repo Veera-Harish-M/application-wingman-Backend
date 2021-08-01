af9eb22232c7216952afb8d3e28d94832bd8484c
Veera Harish Muthazhagu <veeraharishchitra@gmail.com>
2021-08-01 18:08:51 +0530

    get algo changes

--- controllers/algo.js
+++ controllers/algo.js
@@ -1,7 +1,7 @@
-const { words } = require('lodash');
-const Algo = require('../models/algo');
+const { words } = require("lodash");
+const Algo = require("../models/algo");
 
-const { promisify } = require('util');
+const { promisify } = require("util");
 
 exports.addAlgo = (req, res) => {
   try {
@@ -16,15 +16,15 @@ exports.addAlgo = (req, res) => {
       language,
     } = req.body;
 
-    Algo.findOne({name}).exec((err, realgo) => {
+    Algo.findOne({ name }).exec((err, realgo) => {
       if (err)
         return res
           .status(200)
-          .json({status: 'Error', message: 'Something Went Wrong'});
+          .json({ status: "Error", message: "Something Went Wrong" });
       if (realgo) {
         return res.status(400).json({
-          status: 'Error',
-          message: 'Algo name already exixt',
+          status: "Error",
+          message: "Algo name already exixt",
         });
       }
 
@@ -42,126 +42,130 @@ exports.addAlgo = (req, res) => {
       algo.save(async (err, resalgo) => {
         if (err) {
           return res.status(401).json({
-            status: 'Error',
-            message: 'Error saving user in database. Try signup again',
+            status: "Error",
+            message: "Error saving user in database. Try signup again",
           });
         }
         return res.json({
-          status: 'Success',
-          message: 'Algo added Successfully',
-          data: {id: resalgo._id},
+          status: "Success",
+          message: "Algo added Successfully",
+          data: { id: resalgo._id },
         });
       });
     });
   } catch (err) {
     res.status(400).send({
-      status: 'Error',
-      message: 'Something Went Wrong',
+      status: "Error",
+      message: "Something Went Wrong",
     });
   }
 };
 
-
-
-exports.algoSearch = async(req, res) => {
+exports.algoSearch = async (req, res) => {
   try {
     // create query object to hold search value and category value
-    console.log('1');
-    var result=await getAlgo(req.query.search)
+    console.log("1");
+    var result = await getAlgo(req.query.search);
     await delay(1000);
-  console.log(result.length);
-  console.log('2');
+    console.log(result.length);
+    console.log("2");
     res.json({
-        status: 'Success',
-        message: 'Successfully fetched',
-        data: result,
-      });
-    
-    
+      status: "Success",
+      message: "Successfully fetched",
+      data: result,
+    });
   } catch (err) {
-    res.json({status: 'Error', message: 'Something Went Wrong'});
+    res.json({ status: "Error", message: "Something Went Wrong" });
   }
 };
 
-
-
 const delay = promisify(setTimeout);
 
-
-const getAlgo=async (search)=>{
+const getAlgo = async (search) => {
   const query = {};
-    const query1={};
-    let alg=[];
-    let algIds=[];
+  const query1 = {};
+  let alg = [];
+  let algIds = [];
 
-    let words=["is","was","the","me","hi","hello","get","algorithm","a","an","bring","can","you","bot","give","program","code","algo"]
-    // assign search value to query.name
-    if (search) {
-      var temp=search.split(" ")
-      console.log(search);
-      await temp.forEach(async element => {
-        tempWord=element
-          if(!(words.indexOf(tempWord) > -1)){
-            if(element.endsWith("ing")){
-              tempWord=tempWord.substring(0, element.length-3)
-            }
+  let words = [
+    "is",
+    "was",
+    "the",
+    "me",
+    "hi",
+    "hello",
+    "get",
+    "algorithm",
+    "a",
+    "an",
+    "bring",
+    "can",
+    "you",
+    "bot",
+    "give",
+    "program",
+    "code",
+    "algo",
+  ];
+  // assign search value to query.name
+  if (search) {
+    var temp = search.toLowerCase().split(" ");
+    console.log(search);
+    await temp.forEach(async (element) => {
+      tempWord = element;
+      if (!(words.indexOf(tempWord) > -1)) {
+        if (element.endsWith("ing")) {
+          tempWord = tempWord.substring(0, element.length - 3);
+        }
 
-            if(element.endsWith("ed")){
-              tempWord=tempWord.substring(0, element.length-2)
-            }
+        if (element.endsWith("ed")) {
+          tempWord = tempWord.substring(0, element.length - 2);
+        }
 
-            console.log(tempWord);
-            query.name = {$regex: tempWord, $options: 'i'};
-            query1.description = {$regex: tempWord, $options: 'i'};
-      
-            resultsFinal=[]
-            
+        console.log(tempWord);
+        query.name = { $regex: tempWord, $options: "i" };
+        query1.description = { $regex: tempWord, $options: "i" };
 
-            await Algo.find(query, async(err, res1) => {
-              if (err) {
-                console.log(err);
-                return res.status(400).json({
-                  status: 'Error',
-                  message: 'Something Went Wrong',
-                });
-              }
-              resultsFinal= resultsFinal.concat(res1)
+        resultsFinal = [];
+
+        await Algo.find(query, async (err, res1) => {
+          if (err) {
+            console.log(err);
+            return res.status(400).json({
+              status: "Error",
+              message: "Something Went Wrong",
             });
-            await Algo.find(query1, async(err2, res2) => {
-              if (err2) {
-                console.log(err2);
-                return res2.status(400).json({
-                  status: 'Error',
-                  message: 'Something Went Wrong',
-                });
-              }
-
-              resultsFinal= resultsFinal.concat(res2)
-              
-            });
-           
-            await resultsFinal.forEach(element2 => {
-              
-              if(!algIds.includes(JSON.stringify(element2._id))){
-                alg.push(element2)
-                algIds.push(JSON.stringify(element2._id))
-              }
-
+          }
+          resultsFinal = resultsFinal.concat(res1);
+        });
+        await Algo.find(query1, async (err2, res2) => {
+          if (err2) {
+            console.log(err2);
+            return res2.status(400).json({
+              status: "Error",
+              message: "Something Went Wrong",
             });
           }
 
-      })
-      
-    }
-return alg
-}
+          resultsFinal = resultsFinal.concat(res2);
+        });
 
+        await resultsFinal.forEach((element2) => {
+          if (!algIds.includes(JSON.stringify(element2._id))) {
+            alg.push(element2);
+            algIds.push(JSON.stringify(element2._id));
+          }
+        });
+      }
+    });
+  }
+  return alg;
+};
 
 exports.getAlgoByCategory = async (req, res) => {
   try {
-  
-    const categ=req.query.category;
-    await Algo.find({category:categ }).exec((err, algos) => {
+    const categ = req.query.category;
+    await Algo.find({ category: categ }).exec((err, algos) => {
       console.log("hello");
       if (err || !algos) {
         return res.status(400).json({
@@ -171,8 +175,8 @@ exports.getAlgoByCategory = async (req, res) => {
       }
 
       res.json({
-        status: 'Success',
-        message: 'Successfully fetched',
+        status: "Success",
+        message: "Successfully fetched",
         data: algos,
       });
     });
